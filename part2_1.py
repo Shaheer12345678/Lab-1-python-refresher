5b3c17d813c75b59c5bd7715f38a23cd3806c248
shaheer12345678 <shaheershakir123@gmail.com>
2025-01-29 12:20:25 -0700

    part_2.1

--- part2_1.py
+++ part2_1.py
@@ -17,4 +17,8 @@ def do_stuff():
         print('There are no real solutions.')   
 
 do_stuff() 
-print(sys.argv[0])
+#(i)the codes purpose is to compute the quadratic formula given 3 arguments.
+
+# (ii)the errors withen the code are that it is possible for us to have a=0 which results in an error as our code does not account for division by 0.
+# also the code does not check for how many arguments were provided which could lead in failure to run.
+# also the closing apostrophes did not match the opening reseluting in an error
