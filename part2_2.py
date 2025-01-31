5c4a7423da1e1aff21892403846c6ec13779f2a9
Danil-V-uni <danil.vanichkin@ucalgary.ca>
2025-01-31 08:07:28 -0700

    Finish part 2.2

--- part2_2.py
+++ part2_2.py
@@ -2,19 +2,21 @@ file= open("pg2701.txt",'r')
 vowel_count=0
 word_count=0
 content=file.read().split('\n')
+found_line = False
 for i in range(len(content)):
     line=content[i]
-    if line!="CHAPTER 1. Loomings.":
-        
+
+    if found_line or line=="CHAPTER 1. Loomings.":
+        found_line = True
+    else:
+        continue
+
     words=line.split(" ")
     word_count+=len(words)
     for word in words:
         for c in word:
             if c == "a" or c == "A" or c == "e" or c == "E" or c == "o" or c == "O" or c == "i" or c == "I" or c == "i" or c == "y" or c == "Y" or c == "u" or c == "U":
                 vowel_count+=1
-
-         
-
 
 file.close()
 
